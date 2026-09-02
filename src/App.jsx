e5edda17e988aac6388d2fb10c628bdc50ac96fa
anilchary <vadlaanilchary225@gmail.com>
2026-09-02 14:49:44 +0530

    web analytics added

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,20 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
+import { Analytics } from '@vercel/analytics/react';
 
 import Home from "./pages/Home";
 
 function App() {
   return (
+    <>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
+    <Analytics />
+    </>
   );
 }
 
